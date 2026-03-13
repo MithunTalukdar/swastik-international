59805663b8c310aeafc7177927c56379b2ad324d
MithunTalukdar <mithuntalukdar2003@gmail.com>
2026-03-13 20:08:57 +0530

    refactor: update route structure in App component for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<SiteLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/clients" element={<ClientsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
+        <Route path="/" element={<SiteLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="services" element={<ServicesPage />} />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="clients" element={<ClientsPage />} />
+          <Route path="contact" element={<ContactPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
